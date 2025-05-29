4287acbea7f5790d148fea2687780fb8e7a22a26
makmach <471842@ntti.edu.kh>
2025-05-29 18:09:39 +0700

    35. Configure Dotenv and Database Connection

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Foodly Backend is running on port ${port}!`))
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => console.log("Foodly Backend is connected to MongoDB!"))
+  .catch((err) => {err});
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(process.env.PORT || 6013, () =>
+  console.log(`Foodly Backend is running on port ${process.env.PORT || 6013}!`)
+);
