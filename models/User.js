1b05c1a4a85425112e054f3b73dcfb3536418031
makmach <471842@ntti.edu.kh>
2025-06-08 17:25:20 +0700

    55. User Schema

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    otp: { type: String, required: true, default: "none" },
+    password: { type: String, required: true },
+    verification: { type: Boolean, default: false },
+    phone: { type: String, default: "0123456789" },
+    phoneVerification: { type: Boolean, default: false },
+    address: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Address",
+        required: false,
+    },
+    userType: {
+        type: String,
+        required: true,
+        default: "Client",
+        enum: ["Client", "Admin", "Vendor", "Driver"],
+    },
+    profile: {
+        type: String,
+        default: "https://th.bing.com/th/id/OIP.kN8tEO6_wPf1PMEofLrdTgHaGh?rs=1&pid=ImgDetMainhttps://th.bing.com/th/id/OIP.kN8tEO6_wPf1PMEofLrdTgHaGh?rs=1&pid=ImgDetMain",
+    },
+    timestamps: true,
+});
+
+module.exports = mongoose.model("User", UserSchema);
